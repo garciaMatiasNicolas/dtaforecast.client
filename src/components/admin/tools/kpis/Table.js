0aae060a394d535ea3618fda1943b0f4db8262e5
mngar <matiasngarcia2002@gmail.com>
2024-06-03 12:12:55 -0300

    table reports issue

--- src/components/admin/tools/kpis/Table.js
+++ src/components/admin/tools/kpis/Table.js
@@ -14,20 +14,19 @@ const TableReport = ({ props, data }) => {
     setCurrentPage(selected);
   };
 
-  useEffect(()=>{
-    if (data.data !== undefined && data.data.length < 7){
+  useEffect(() => {
+    if (data.data !== undefined && data.data.length < 7) {
       setCurrentPage(0);
     }
-  }, [props])
-  
+  }, [props, data.data]);
 
   if (!data || !data.data) {
-    return( 
-    <div>
-      <p>Selecciona todos los filtros para visualizar los datos... </p>
-      <p>(Nota: Los escenarios mostrados son para el proyecto seleccionado)</p>
-    </div>
-    )
+    return (
+      <div>
+        <p>Selecciona todos los filtros para visualizar los datos... </p>
+        <p>(Nota: Los escenarios mostrados son para el proyecto seleccionado)</p>
+      </div>
+    );
   }
 
   // Separar la última fila de la tabla
@@ -39,7 +38,7 @@ const TableReport = ({ props, data }) => {
         <MDBTableHead className='bg-primary'>
           <tr>
             <th scope='col' className='text-white'>{props}</th>
-            {data.length === 0 ? (
+            {data.columns.length === 0 ? (
               <>
                 <th scope='col'></th>
                 <th scope='col'></th>
@@ -47,29 +46,28 @@ const TableReport = ({ props, data }) => {
                 <th scope='col'></th>
               </>
             ) : (
-              data.columns.map((year) => <th scope='col' className='text-white'>{year}</th>)
+              data.columns.map((year, index) => <th key={index} scope='col' className='text-white'>{year}</th>)
             )}
           </tr>
         </MDBTableHead>
         <MDBTableBody>
           {currentPageData.slice(0, -1).map((rowData, index) => (
             <tr className='w-auto' key={index}>
-              <th scope='row' className='w-auto border'>{rowData[0] === 0 ? `Sin ${props}` : rowData[0]}</th>
-              {rowData.slice(1).map((value, yearIndex) => (
-                <td className='w-auto border' key={yearIndex}>{value}</td>
+              <th scope='row' className='w-auto border'>{rowData.name === 0 ? `Sin ${props}` : rowData.name}</th>
+              {data.columns.map((year, yearIndex) => (
+                <td className='w-auto border' key={yearIndex}>{rowData[year]}</td>
               ))}
             </tr>
           ))}
           <tr className='bg-success w-auto'>
-            <th scope='row' className='text-white'>{lastRow[0]}</th>
-            {lastRow.slice(1).map((value, yearIndex) => (
-              <td className='text-white' key={yearIndex}>{value}</td>
+            <th scope='row' className='text-white'>{lastRow.name}</th>
+            {data.columns.map((year, yearIndex) => (
+              <td className='text-white' key={yearIndex}>{lastRow[year]}</td>
             ))}
           </tr>
         </MDBTableBody>
       </MDBTable>
-      {
-        data.data.length >= 6 && 
+      {data.data.length >= 6 && (
         <ReactPaginate
           previousLabel={<MDBIcon fas icon="angle-double-left" />}
           nextLabel={<MDBIcon fas icon="angle-double-right" />}
@@ -83,7 +81,7 @@ const TableReport = ({ props, data }) => {
           pageClassName={'page-item'}
           activeClassName={'active text-decoration-underline'}
         />
-      }
+      )}
     </div>
   );
 };
